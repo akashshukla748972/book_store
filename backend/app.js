027f4ca8aef438ec2a2c01a1ff4dd8b7c1ee5a9c
akashshukla748972 <shuklaakash748972@gmail.com>
2025-03-30 14:53:06 +0530

    add global error handler middleware in exit point

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,15 @@
 import express from "express";
+import globalErrorHandler from "./middlewares/errorHandler.js";
+import CustomError from "./utils/customError.js";
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.status(200).json({
-    message: `Welcome in our book store!`,
-  });
+app.use("*", (req, res, next) => {
+  next(CustomError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
+
+app.use(globalErrorHandler);
 
 export default app;
