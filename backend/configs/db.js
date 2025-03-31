27a37b6aa72369328aac0d4943188e8a9dff5ef1
akashshukla748972 <shuklaakash748972@gmail.com>
2025-03-31 18:26:10 +0530

    chage mongo db local to atlas

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -3,7 +3,9 @@ import gb from "./globalVariable.js";
 
 const connectToDb = async () => {
   try {
-    await mongoose.connect(gb.mongoDbUrl);
+    await mongoose.connect(
+      `mongodb+srv://${gb.mongoDbUser}:${gb.mongoDbPassword}@cluster0.gsvplee.mongodb.net/book_store?retryWrites=true&w=majority&appName=Cluster0`
+    );
     console.log(`Database connected successfully`);
   } catch (error) {
     console.error(`Error while connecting to database ${error}`);
--- backend/configs/globalVariable.js
+++ backend/configs/globalVariable.js
@@ -4,6 +4,8 @@ dotenv.config();
 const gb = {
   port: process.env.PORT,
   mongoDbUrl: process.env.MONGODB_URL,
+  mongoDbUser: process.env.MONGODB_USER,
+  mongoDbPassword: process.env.MONGODB_PASSWORD,
   jwtSecret: process.env.JWT_SECRET,
 };
 
